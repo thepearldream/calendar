194a48abccce04a808354af5313ef11b1e87b27b
Franco Abott <franco.abott@thepearldream.com>
2017-01-15 16:42:00 -0500

    configure facebook login and calendar module

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -1,14 +1,59 @@
 'use strict';
 
-angular.module('myApp.view1', ['ngRoute'])
+angular.module('myApp.view1', ['facebookUtils', 'ngRoute'])
+// angular.module('myApp.view1', ['ngRoute'])
+
+.constant('facebookConfigSettings', {
+    'routingEnabled' : true,
+    'channelFile'    : 'channel.html',
+    'appID'          : '178504295959533'
+  })
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'CalendarCtrl'
   });
 }])
 
-.controller('View1Ctrl', [function() {
+.controller('CalendarCtrl', [function() {
+  $rootScope.loggedInUser = {};
 
+    $rootScope.$on('fbLoginSuccess', function(name, response) {
+      facebookUser.then(function(user) {
+        user.api('/me').then(function(response) {
+          $rootScope.loggedInUser = response;
+        });
+      });
+    });
+
+    $rootScope.$on('fbLogoutSuccess', function() {
+      $scope.$apply(function() {
+        $rootScope.loggedInUser = {};
+      });
+    });
 }]);
+
+// angular.module('facebookUtilsDemo', ['facebookUtils', 'ngRoute'])
+//   .constant('facebookConfigSettings', {
+//     'routingEnabled' : true,
+//     'channelFile'    : 'channel.html',
+//     'appID'          : '178504295959533'
+//   })
+//   .controller('RootCtrl', function($rootScope, $scope, facebookUser) {
+//     $rootScope.loggedInUser = {};
+
+//     $rootScope.$on('fbLoginSuccess', function(name, response) {
+//       facebookUser.then(function(user) {
+//         user.api('/me').then(function(response) {
+//           $rootScope.loggedInUser = response;
+//         });
+//       });
+//     });
+
+//     $rootScope.$on('fbLogoutSuccess', function() {
+//       $scope.$apply(function() {
+//         $rootScope.loggedInUser = {};
+//       });
+//     });
+//   });
--- app/view2/view2.js
+++ app/view2/view2.js
@@ -1,11 +1,13 @@
 'use strict';
 
-angular.module('myApp.view2', ['ngRoute'])
+angular.module('myApp.view2', ['mwl.calendar', 'ui.bootstrap','ngRoute'])
+// angular.module('myApp.view2', ['ngRoute'])
 
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view2', {
     templateUrl: 'view2/view2.html',
-    controller: 'View2Ctrl'
+    controller: 'View2Ctrl',
+    needAuth: true
   });
 }])
 
