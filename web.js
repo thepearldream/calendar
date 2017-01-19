f49d7d84c83d67f237c3d7e534cac2c14bab598d
Franco Abott <franco.abott@thepearldream.com>
2017-01-18 23:18:17 -0500

    express web server for heroku deploy

---
+++ web.js
@@ -0,0 +1,9 @@
+'use strict';
+
+var gzippo = require('gzippo');
+var express = require('express');
+var nodeApp = express();
+
+nodeApp.use(express.logger('dev'));
+nodeApp.use(gzippo.staticGzip('' + __dirname + '/app'));
+nodeApp.listen(process.env.PORT || 5000);
