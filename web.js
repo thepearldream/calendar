6c5ac222c6306fbfe9f82b40bbe935c5b018b3a1
Franco Abott <franco.abott@thepearldream.com>
2017-01-18 23:58:19 -0500

    remove use of gzippo

--- web.js
+++ web.js
@@ -5,5 +5,6 @@ var express = require('express');
 var nodeApp = express();
 
 nodeApp.use(express.logger('dev'));
-nodeApp.use(gzippo.staticGzip('' + __dirname + '/app'));
+// nodeApp.use(gzippo.staticGzip('' + __dirname + '/app'));
+nodeApp.use('' + __dirname + '/app');
 nodeApp.listen(process.env.PORT || 5000);
